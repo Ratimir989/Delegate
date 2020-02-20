c57120745b0ae2bc219ec7856cc21f6871bd5465
Ratimir <ra0953029364@gmail.com>
2020-02-20 16:31:19 +0200

    Initial commit

---
+++ delegate_1/delegate_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace delegate_1
+{
+    class Program
+    {
+        delegate int AvarageHandler(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            AvarageCalculator();
+            Console.ReadLine();
+        }
+
+        static void AvarageCalculator()
+        {
+            try
+            {
+                Console.WriteLine("Bведите три числа через ПРОБЕЛ:");
+                string[] numbers = Console.ReadLine().Split(' ');
+
+                AvarageHandler avarageHandler = (a, b, c) => { return (a + b + c) / 3; };
+
+                Console.WriteLine("Среднее арифметическое этих чисел = " + avarageHandler(Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1]), Convert.ToInt32(numbers[2])));
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод! Попроуйте ещё раз.");
+                AvarageCalculator();
+            }
+        }
+    }
+}
---
+++ delegate_3/delegate_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace delegate_3
+{
+    class Program
+    {
+        delegate int Rnd();
+        delegate double Avarage(Rnd[] r);
+        static int MyRandom()
+        {
+            Random rand = new Random();
+            return rand.Next(1000);
+        }
+        static void Main(string[] args)
+        {
+            Avarage av = delegate (Rnd[] r)
+            {
+                int sum = 0;
+                for (int i = 0; i < r.Length; i++)
+                    sum += r[i].Invoke();
+                return sum / r.Length;
+            };
+            Console.Write(av(new Rnd[3] { new Rnd(MyRandom), new Rnd(MyRandom), new Rnd(MyRandom) }));
+            Console.ReadKey();
+        }
+    }
+}
